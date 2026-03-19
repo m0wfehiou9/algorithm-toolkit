9805b5ead721e4f97446aa213ab00d12f0819578
m0wfehiou9 <m0lckrr@gmail.com>
2026-03-19 22:54:57 +0000

    Create contraintes.py

---
+++ contraintes.py
@@ -0,0 +1,6 @@
+#contraintes a prendre en compte
+# profs a temps plein vs profs avec des horraires fixes 
+#algo doit etre capable de generer tout un semestre (13semaines/720h de cours )
+#4 salles de capacites differentes
+#eviter les heures creuses entre 2 matieres
+
