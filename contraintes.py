f47c0f53353c66f9b902043f6ef9cc38788f20e6
m0wfehiou9 <m0lckrr@gmail.com>
2026-03-30 16:59:15 +0100

    Add professor availability and semester scheduling logic

--- contraintes.py
+++ contraintes.py
@@ -1,6 +1,20 @@
 #contraintes a prendre en compte
 # profs a temps plein vs profs avec des horraires fixes 
+disponibilites_profs = {
+    "H. NAQOS": set(range(len(slots))),
+    "S. HDAFA": set(range(len(slots))),
+
+    "S. EL FASSI": {0, 1, 4, 5},
+    "A. BENKHRABA": {8, 9, 10, 11},
+    "S. SHLAKA": {2, 3, 6, 7},
+}
 #algo doit etre capable de generer tout un semestre (13semaines/720h de cours )
+for semaine in range(13):
+    emploi_semaine = coloration_graph_with_retries(graph)
+    emplois_semestre.append(emploi_semaine)
 #4 salles de capacites differentes
+
 #eviter les heures creuses entre 2 matieres
+
+#doit generer un pdf de 13semaines par classe
 
