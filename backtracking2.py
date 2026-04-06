73b547bd81f08348fe9cf98b35d4961b6f9d5f94
m0wfehiou9 <m0lckrr@gmail.com>
2026-04-06 14:11:40 +0100

    Create backtracking2.py

---
+++ backtracking2.py
@@ -0,0 +1,40 @@
+	⁠H.NAQOS, algèbre2, jm1, 6, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+	⁠H.NAQOS, analyse2, jm1, 6, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+	⁠H.NAQOS, mathématique, hei & isen, 15, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
+	⁠H.NAQOS, analyse numérique, jm2, 14, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+	⁠H.NAQOS, apprentissage automatique, jm4, 8, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+	⁠S.HDAFA, algorithme et programmation, jm1, 25, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+	⁠S.HDAFA, développement web, jm2, 28, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
+	⁠S.HDAFA, projet étude et conception, isen, 16, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+	⁠H.ZEROUALI, analyse2, jm1, 6, 0 1 12 13 16 17 18 
+	⁠H.ZEROUALI, algèbre2, jm1, 6, 0 1 12 13 16 17 18 
+	⁠H.ZEROUALI, probabilite, jm2, 24,0 1 12 13 16 17 18 
+	⁠A.DAHMANI, science de l’ingénieur, jm2, 16, 4 5 8 9 16 17 18
+	⁠A.DAHMANI, projet étude et conception, hei, 16, 4 5 8 9 16 17 18
+	⁠A.DAHMANI, ingénierie mécanique, hei, 24, 4 5 8 9 16 17 18
+	⁠A.ELMANSOUR, interculturalité, hei & isen, 9, 2 3 14 15
+	⁠A.ELYAZIDI, aide à la decision, hei & isen, 18, 0 1 9 10 16 17
+	⁠A.ELYAZIDI, système d’exploitation, jm4, 12, 0 1 9 10 16 17
+	⁠T.AKDIM, intro gestion compta finance, jm1, 15, 0 1 2 3 6 7 10 11
+	⁠T.AKDIM, marketing, hei & isen, 9, 0 1 2 3 6 7 10 11
+	⁠T.AKDIM, entreprenariat, jm4, 12, 0 1 2 3 6 7 10 11
+	⁠S.ELFASSI, mécanique générale, jm1, 24, 4 5 8 9 16 17
+	⁠S.ELFASSI, mécanique quantique, isen, 18, 4 5 8 9 16 17
+	⁠S.ELFASSI, mécanique des fluides, jm2, 12, 4 5 8 9 16 17
+	⁠A.BENKHRABA, anglais1, jm1, 12, 2 3 10 19
+	⁠A.BENKHRABA, anglais2, jm2, 12, 2 3 10 19
+	⁠D.GUENDOUZ, projet collectif découverte domaine, jm1, 12, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
+	⁠D.GUENDOUZ, communication for working, jm2, 12, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
+	⁠S.SHLAKA, communication1, jm1, 12, 6 7 14 15
+	⁠S.SHLAKA, communication2, jm2, 12, 6 7 14 15
+	⁠N.AHAMI, thermodynamique, jm1, 14, 10 11 12 13 16 17
+	⁠C.BAQA, science de l’ingénieur, jm1, 14, 16 17
+	⁠M.SEBGUI, systèmes embarqués, jm2, 24, 4 5 8 9 14 15
+	⁠S.MOULINE, BD et BD Avancées, jm4, 24, 0 1 8 9 
+	⁠M.RZIZA, administration et sécurité des réseaux, jm4, 24, 0 1 8 9 12 13
+	⁠Y.AKALAY, anglais, jm4, 12, 16 17
+	⁠M.ELHASSOUNI, big data et analyse distribuée des données, jm4, 24, 6 7 8 9 15 16 
+	⁠A.ESSADKI, principe de l’instrumentation, hei & isen, 18, 6 7 10 11 12 13 14 15 
+	⁠A.ESSADKI, automatisation2, hei & isen, 18, 6 7 10 11 12 13 14 15 
+	⁠A.ESSADKI, electrecite vecteur d’énergie , hei , 18, 6 7 10 11 12 13 14 15
+	⁠A.ESSADKI, regulation industrielle, isen, 18, 6 7 10 11 12 13 14 15
